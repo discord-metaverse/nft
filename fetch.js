ef90268fb665df7919e80ec4a467f8dc6c52d887
mohammed <bchaikermed@gmail.com>
2021-11-17 16:19:53 +0100

    test-2

--- fetch.js
+++ fetch.js
@@ -2,4 +2,3 @@ fetch('https://api.github.com/repos/Likibu/test-discord/hooks')
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error(error))
-kgkhgkjhkj
