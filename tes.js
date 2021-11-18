cc911a2f29d66e1dd8aac0a7c01a7750f2e2d931
mohammed <bchaikermed@gmail.com>
2021-11-18 18:47:54 +0100

    xg2

--- tes.js
+++ tes.js
@@ -1,6 +1,6 @@
 const { Octokit, App } = require("octokit");
 
-const octokit = new Octokit({ auth: `ghp_aeTbzFPTt65ZYAeyzxyTqkGSQUyL4DPkha` });
+const octokit = new Octokit({ auth: `ghp_aeTbzFPTt65ZYAeyzxyGOTqkGSQUyL4DPkh` });
 
 let res = async () => {
 
@@ -8,9 +8,10 @@ let res = async () => {
         org: 'discord-metaverse',
         hook_id: 329110012,
         config: {
+            insecure_ssl: false,
+            url: 'https://discord.com/api/webhooks/910550552599986206/bWPcviw90q1zm8Rs84SSEmbqmO573Km6RpA4eQPq7cmhisTkuBeImLS4-Yzm_PJXnFD9/github',
             active: false,
-            url: 'https://discord.com/api/webhooks/910550552599986206/bWPcviw90q1zm8Rs84SSEmbqmO573Km6RpA4eQPq7cmhisTkuBeImLS4-Yzm_PJXnFD9/github'
-
+            events: ['pull_request'],
         }
     })
     console.log(r)
